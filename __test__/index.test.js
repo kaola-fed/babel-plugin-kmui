5dba7fd5fb5b133ea7216896fa381de9826f03f1
MO <fengzilong1992@gmail.com>
2017-06-21 07:12:14 +0800

    test: Add test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,17 @@
 const path = require( 'path' );
+const fs = require( 'fs' );
 const { transformFileSync } = require( 'babel-core' );
 const plugin = require.resolve( '../lib' );
 
-const code = transformFileSync( path.resolve( __dirname, 'fixtures/index.js' ), {
-	presets: [ [
-		'es2015',
-	] ],
-	plugins: [ [ plugin, { libraryName: 'kmui' } ] ],
-} ).code;
+test( 'should transform import decl correctly', function () {
+	const code = transformFileSync( path.resolve( __dirname, 'fixtures/index.js' ), {
+		// presets: [ [
+		// 	'es2015',
+		// ] ],
+		plugins: [ [ plugin, { libraryName: 'kmui' } ] ],
+	} ).code;
 
-console.log( code );
+	const expected = fs.readFileSync( path.join( __dirname, 'fixtures/expected.js' ) );
+
+	expect( code.trim() ).toBe( expected.toString().trim() );
+} );
