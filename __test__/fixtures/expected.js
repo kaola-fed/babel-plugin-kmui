bd760fdae98589323748975c1b59770e7ec3fd25
MO <fengzilong1992@gmail.com>
2017-07-18 22:17:46 +0800

    Fix broken tests

--- __test__/fixtures/expected.js
+++ __test__/fixtures/expected.js
@@ -1,9 +1,6 @@
 import _KMUI_TMP_ from 'kmui/lib/components/modal';
-import 'kmui/lib/components/modal/index.mcss';
 import _KMUI_TMP_2 from 'kmui/lib/components/button';
-import 'kmui/lib/components/button/index.mcss';
 import _KMUI_TMP_3 from 'kmui/lib/components/sku-select';
-import 'kmui/lib/components/sku-select/index.mcss';
 import KMUI_TMP_install_ from 'kmui/lib/install';
 import 'kmui/lib/index.mcss';
 const SkuSelect = KMUI_TMP_install_(_KMUI_TMP_3);
